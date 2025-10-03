483aae511e0011d787060a3c8e18cd05abde4f34
nishuldhakar <nishuldhakar.se@gmail.com>
2025-10-03 11:55:38 +0530

    updated blog

--- src/data/About.tsx
+++ src/data/About.tsx
@@ -1,5 +1,5 @@
 export const about = {
   name: "Nishul Dhakar",
   description:
-    "I’m Nishul, a final-year B.Tech Software Developer passionate about building modern web applications. My expertise includes Next.js, React, TypeScript, Tailwind CSS, along with backend development using Java, Spring Boot, and Express. I also have strong knowledge of databases (SQL, MongoDB) and practice Data Structures & Algorithms regularly on LeetCode with Java.",
+    "I’m Nishul, passionate about crafting modern web experiences. I love building fast, scalable applications with clean design and smooth user interactions.",
 };
